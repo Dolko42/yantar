1c6cb12c06e1a7b7003a342c2b31c2d83c580113
Dolko42 <dolkodolkoo@gmail.com>
2024-05-20 17:19:46 +0200

    home page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,17 +47,19 @@ export default function Home() {
         </section>
 
         <section
-          className="flex flex-col lg:grid lg:grid-cols-3 lg:grid-rows-3 gap-8 py-10 max-w-[80rem]"
+          className="flex flex-col lg:grid lg:grid-cols-3 lg:grid-rows-3 gap-8 py-10"
           style={{ margin: "0 auto" }}
         >
           <div className="flex flex-col justify-between h-56 text-skin-secondary p-4 bg-skin-secondarymuted border border-skin-secondary rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">advertisers</span>
-              <h3 className="text-xl max-w-[20ch] mt-1">
+              <span className="uppercase font-mono text-sm 2xl:text-md">
+                advertisers
+              </span>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
                 Drive relevant traffic
               </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
               Reach your target audience effectively with Yantar’s
               context-driven ad placements.
             </p>
@@ -65,10 +67,14 @@ export default function Home() {
 
           <div className="flex flex-col justify-between h-56 text-skin-secondary p-4 bg-skin-secondarymuted border border-skin-secondary rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">advertisers</span>
-              <h3 className="text-xl max-w-[20ch] mt-1">Pay per click.</h3>
+              <span className="uppercase font-mono text-sm 2xl:text-md">
+                advertisers
+              </span>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
+                Pay per click.
+              </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
               You only pay when users engage with your ads, ensuring
               cost-efficiency and measurable ROI.
             </p>
@@ -76,12 +82,14 @@ export default function Home() {
 
           <div className="flex flex-col justify-between h-56 text-skin-secondary p-4 bg-skin-secondarymuted border border-skin-secondary rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">advertisers</span>
-              <h3 className="text-xl max-w-[20ch] mt-1">
+              <span className="uppercase font-mono text-sm 2xl:text-md">
+                advertisers
+              </span>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
                 Add AI platforms to your omnichannel strategy.
               </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
               Easily integrate AI-driven ads into your broader marketing
               efforts.
             </p>
@@ -89,12 +97,14 @@ export default function Home() {
 
           <div className="flex flex-col justify-between h-56 text-skin-base p-4 bg-skin-subtle border border-skin-muted rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">AI platforms</span>
-              <h3 className="text-xl max-w-[20ch] mt-1">
+              <span className="uppercase font-mono text-sm 2xl:text-md">
+                AI platforms
+              </span>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
                 Enhance user experience with relevant ads.
               </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
               Improve user satisfaction by displaying ads that are contextually
               relevant.
             </p>
@@ -102,10 +112,14 @@ export default function Home() {
 
           <div className="flex flex-col justify-between h-56 text-skin-base p-4 bg-skin-subtle border border-skin-muted rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">AI platforms</span>
-              <h3 className="text-xl max-w-[20ch] mt-1">Get paid per click.</h3>
+              <span className="uppercase font-mono text-sm 2xl:text-md">
+                AI platforms
+              </span>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
+                Get paid per click.
+              </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
               With Yantar, you earn revenue for every ad click generated through
               your platform, providing a consistent income stream.
             </p>
@@ -113,10 +127,14 @@ export default function Home() {
 
           <div className="flex flex-col justify-between h-56 text-skin-base p-4 bg-skin-subtle border border-skin-muted rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">AI platforms</span>
-              <h3 className="text-xl max-w-[20ch] mt-1">Integrate easily.</h3>
+              <span className="uppercase font-mono text-sm 2xl:text-md">
+                AI platforms
+              </span>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
+                Integrate easily.
+              </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
               Set up quickly with a unique API key and start fetching relevant
               ads seamlessly.
             </p>
@@ -124,45 +142,47 @@ export default function Home() {
 
           <div className="flex flex-col justify-between h-56 text-gray-800 p-4 bg-gray-100 border border-gray-300 rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">
+              <span className="uppercase font-mono text-sm 2xl:text-md">
                 core principle
               </span>
-              <h3 className="text-xl max-w-[20ch] mt-1">Revenue Sharing</h3>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
+                Revenue Sharing
+              </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
-              AI platforms that will use Yantar can earn revenue by providing
-              advertising space. Advertisers pay to have their ads streamed into
-              the AI interactions, and a portion of this revenue is shared with
-              the AI platforms hosting the ads.
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
+              Advertisers pay to have their ads streamed into the AI
+              interactions, and a portion of this revenue is shared with the AI
+              platforms hosting the ads.
             </p>
           </div>
 
           <div className="flex flex-col justify-between h-56 text-gray-800 p-4 bg-gray-100 border border-gray-300 rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">
+              <span className="uppercase font-mono text-sm 2xl:text-md">
                 core principle
               </span>
-              <h3 className="text-xl max-w-[20ch] mt-1">Semantic Analysis</h3>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
+                Semantic Analysis
+              </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
               When a user interacts with an AI chat, Yantar performs a semantic
-              analysis of the {`user's`} input. This involves understanding the
-              context, intent, and specific nuances of the prompt.
+              analysis of the prompt and selects relevant ads.
             </p>
           </div>
 
           <div className="flex flex-col justify-between h-56 text-gray-800 p-4 bg-gray-100 border border-gray-300 rounded-md">
             <div>
-              <span className="uppercase font-mono text-sm">
+              <span className="uppercase font-mono text-sm 2xl:text-md">
                 core principle
               </span>
-              <h3 className="text-xl max-w-[20ch] mt-1">Ad Streaming</h3>
+              <h3 className="text-xl max-w-[20ch] mt-1 2xl:text-2xl">
+                Ad Streaming
+              </h3>
             </div>
-            <p className="text-sm max-w-[32ch]">
+            <p className="text-sm max-w-[32ch] 2xl:text-[16px]">
               The selected ads are then seamlessly streamed into the
-              AI-generated text or interaction. This integration ensures that
-              the ads appear naturally within the conversation or content,
-              enhancing relevance and user engagement.
+              AI-generated text or interaction.
             </p>
           </div>
         </section>
